8cc6c78265f0c99e79de4e61010982488f5891cf
Ranye Drebes <drebes@simplethings.de>
2024-02-16 12:03:17 +0100

    User management and Profile Page update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,31 +1,96 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React, { useState, useEffect, useContext } from 'react';
+import { UserAuth } from '../context/AuthContext';
+import { auth, db, onAuthStateChanged } from '../firebase';
+import { collection, query, where, getDocs, addDoc, doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
+    
+    const { user } = UserAuth();
+    const collectionUserRef = collection(db, "users");
 
-    const { user, userData } = useContext(AuthContext) ;
+    const [profileData, setProfileData] = useState({
+        username: '',
+        email: '',
+        dateOfBirth: '',
+        password: '',
+        address: '',
+        country: '',
+        region: '',
+    });
 
-    const handleSaveProfile = () => {
-      // Add logic to save the updated profile data
-      // You can use the 'data' state to get the updated values
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged( auth, user => {
+          if (user) {
+            loadProfile(user.uid);
+          }
+        });
+    
+        return () => unsubscribe();
+      }, []);
+    
+      const loadProfile = async (uid) => {
+        try {
+          const q = query(collectionUserRef, where('uid', '==', uid));
+          const querySnapshot = await getDocs(q);
+    
+          if (!querySnapshot.empty) {
+            const userData = querySnapshot.docs[0].data();
+            setProfileData(userData);
+            console.log('Loaded profile data:', userData);
+          } else {
+            console.log('No matching documents for the user');
+          }
+        } catch (error) {
+          console.error('Error loading profile data:', error);
+        }
+      };
+  
+    const handleSaveProfile = async () => {
+        try {
+            const q = query(collectionUserRef, where("uid", "==", user.uid));
+            const docs = await getDocs(q);
+
+        if (docs.docs.length !== 0) {
+            const userDocRef = docs.docs[0].ref;
+
+            // Extract fields that are not empty from profileData
+            const updatedProfileData = Object.fromEntries(
+                Object.entries(profileData)
+            );
+
+            await updateDoc(userDocRef, updatedProfileData);
+            console.log('Profile data updated successfully');
+        } else {
+            console.log("User not found!");
+        }
+        } catch (error) {
+            console.error('Error updating profile data:', error);
+        }
     };
   
-    return (
-        <div className="container d-flex justify-content-center align-items-center">
+    const handleInputChange = (e) => {
+            const { name, value } = e.target;
+            setProfileData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+return (
+    <div className="container d-flex justify-content-center align-items-center">
         <div className="card">
-        <div className="upper">
+            <div className="upper">
         </div>
 
         <div className="user text-center">
             <div className="profile">
-            <img src={userData?.photoURL || user?.photoURL} className="rounded-circle" width="80" alt="Profile" />
+            <img src={user?.photoURL} className="rounded-circle" width="80" alt="Profile" />
             </div>
-            <p>{userData?.displayName || user?.displayName}</p>
+            <p>{user?.displayName}</p>
         </div>
 
         <div className="mt-5 text-center">
             <h4 className="mb-0"></h4>
-
             <div className="d-flex justify-content-between align-items-center mt-4 px-4">
             <div className="stats">
                 <h6 className="mb-0">Followers</h6>
@@ -48,49 +113,55 @@ const Profile = () => {
             <div className="d-flex justify-content-between align-items-center mb-3">
             <h4 className="text-right">Profile Settings</h4>
             </div>
+        
+            <div className="row mt-2">
+            <div className="col-md-6">
+                <label className="labels">Username</label>
+                <input type="text" className="form-control" placeholder={""} name="username" value={profileData?.username} onChange={handleInputChange} />
+            </div> 
+            </div>
             <div className="row mt-2">
             <div className="col-md-6">
                 <label className="labels">Email</label>
-                <input type="text" className="form-control" placeholder={userData?.email || user?.email} />
+                <input required type="text" className="form-control" placeholder={""} name="email" value={profileData?.email} onChange={handleInputChange} />
             </div>
             <div className="col-md-6">
                 <label className="labels">Date of Birth</label>
-                <input type="date" className="form-control" placeholder="" />
+                <input type="date" className="form-control" placeholder={""} name="dateOfBirth" value={profileData?.dateOfBirth} onChange={handleInputChange} />
             </div>
             </div>
             <div className="row mt-2">
             <div className="col-md-6">
                 <label className="labels">Password</label>
-                <input type="password" className="form-control" placeholder="" />
+                <input type="password" className="form-control" placeholder={""} name="password" value={profileData?.password} onChange={handleInputChange} />
             </div>
             <div className="col-md-6">
                 <label className="labels">Repeat Password</label>
-                <input type="password" className="form-control" placeholder="" />
+                <input type="password" className="form-control" placeholder={""} name="password" value={""} onChange={handleInputChange} />
             </div>
             </div>
             <div className="row mt-3">
             <div className="col-md-12">
                 <label className="labels">Address</label>
-                <input type="text" className="form-control" placeholder="" />
+                <input type="text" className="form-control" placeholder={""} name="address" value={profileData?.address} onChange={handleInputChange} />
             </div>
             <div className="col-md-6">
                 <label className="labels">Country</label>
-                <input type="text" className="form-control" placeholder="" />
+                <input type="text" className="form-control" placeholder={""} name="country" value={profileData?.country} onChange={handleInputChange} />
             </div>
             <div className="col-md-6">
                 <label className="labels">State/Region</label>
-                <input type="text" className="form-control" placeholder="" />
+                <input type="text" className="form-control" placeholder={""} name="region" value={profileData?.region} onChange={handleInputChange} />
             </div>
             </div>
             <div className="mt-5 text-center">
-            <button className="btn btn-primary profile-button" type="button">
+            <button className="btn btn-primary profile-button" type="button" onClick={handleSaveProfile}>
                 Save Profile
             </button>
             </div>
         </div>
-        </div>
     </div>
-    )
-};
+</div>
+)};
 
 export default Profile;
