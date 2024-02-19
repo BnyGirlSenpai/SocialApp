e6c51995c82037ecc5e71875f21547a273913965
Ranye Drebes <drebes@simplethings.de>
2024-02-19 10:19:10 +0100

    Map added

--- src/Pages/MapPage.jsx
+++ src/Pages/MapPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Map from '../components/Map';
+import MapContent from '../components/Map';
 import Navbar from '../components/Navbar';
 
 function Mappage() {
   return (
     <div className='Main'>
        <Navbar />
-       <Map />
+       <MapContent />
     </div>
   )
 };
