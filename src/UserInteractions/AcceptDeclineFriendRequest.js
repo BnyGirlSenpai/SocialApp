4994e588fd87eb6609a411596aaf066ed0226a73
Ranye Drebes <drebes@simplethings.de>
2024-02-23 13:33:58 +0100

    Code Effeciency Tests/Improvment

--- src/UserInteractions/AcceptDeclineFriendRequest.js
+++ src/UserInteractions/AcceptDeclineFriendRequest.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { db, auth } from '../firebase';
 import { UserAuth } from '../context/AuthContext';
-import { collection, query, where, getDocs,updateDoc,arrayUnion } from 'firebase/firestore';
+import { collection, query, where, doc, getDoc, updateDoc, arrayUnion } from 'firebase/firestore';
 
 const AcceptRejectFriendRequest = () => {
     const { user } = UserAuth();
@@ -10,12 +10,16 @@ const AcceptRejectFriendRequest = () => {
 
     const getPendingFriendRequests = async (uid) => {
         try {
-            const q = query(collectionUserRef, where('uid', '==', uid));
-            const querySnapshot = await getDocs(q);
-            // Assuming each document has a field called 'friendRequests'
-            const userData = querySnapshot.docs[0].data();
-            const PendingFriendRequests = userData.PendingFriendRequests|| [];
-            setPendingFriendRequests(PendingFriendRequests);
+            const docRef = doc(collectionUserRef, uid);
+            const docSnapshot = await getDoc(docRef);
+            
+            if (docSnapshot.exists()) {
+                const userData = docSnapshot.data();
+                const PendingFriendRequests = userData.PendingFriendRequests || [];
+                setPendingFriendRequests(PendingFriendRequests);
+            } else {
+                console.log("User not found");
+            }
         } catch (error) {
             console.error('Error querying Firestore:', error);
         }
@@ -29,83 +33,63 @@ const AcceptRejectFriendRequest = () => {
         };
     }, [collectionUserRef, user]);
 
-    const acceptFriendRequest = async (uid) => {
-      try {
-        const q = query(collectionUserRef, where('uid', '==', uid));
-        const querySnapshot = await getDocs(q);
-     
-        if (querySnapshot.empty) {
-            console.log("Failed to accept Friend Request");
-          return;
+    const updateFriendRequestStatus = async (uid, status) => {
+        try {
+            const docRef = doc(collectionUserRef, uid);
+            await updateDoc(docRef, {
+                PendingFriendRequests: arrayUnion({ status }),
+            });
+        } catch (error) {
+            console.error(`Error ${status.toLowerCase()}ing friend request:`, error);
         }
-        const targetUserData = querySnapshot.docs[0];
-        await updateDoc(targetUserData.ref, {
-          PendingFriendRequests: arrayUnion({status: 'accepted'}),
-        });
-      } catch (error) {
-        console.error('Error accepting friend request:', error);
-      }
     };
 
-    const rejectFriendRequest = async (uid) => {
-      try {
-        const q = query(collectionUserRef, where('uid', '==', uid));
-        const querySnapshot = await getDocs(q);
-       
-        if (querySnapshot.empty) {
-          console.log("Failed to reject Friend Request");
-          return;
-        }
-        const targetUserData = querySnapshot.docs[0];
-
-        await updateDoc(targetUserData.ref, {
-          PendingFriendRequests: arrayUnion({status: 'rejected'}),
-        });
-      } catch (error) {
-        console.error('Error rejecting friend request:', error);
-      }
+    const acceptFriendRequest = (uid) => {
+        updateFriendRequestStatus(uid, 'accepted');
     };
 
-    return (
-    <div>
-        <div className="container">
-            <div className="row">
-            <div className="col-md-8">
+    const rejectFriendRequest = (uid) => {
+        updateFriendRequestStatus(uid, 'rejected');
+    };
+return (
+<div>
+    <div className="container">
+        <div className="row">
+        <div className="col-md-8">
+            <div>
+            {pendingFriendRequests.length > 0 ? (
                 <div>
-                {pendingFriendRequests.length > 0 ? (
-                    <div>
-                        <h2>Pending Friend Requests</h2>
-                        <ul>
-                            {pendingFriendRequests.map((PendingFriendRequests) => (
-                                <div key={PendingFriendRequests.uid} className="nearby-user">
-                                    <div className="row">
-                                        <div className="col-md-2 col-sm-2">
-                                            <img src={PendingFriendRequests.image} alt="friend" className="profile-photo-lg" />
-                                        </div>
-                                        <div className="col-md-7 col-sm-7">
-                                            <h5>
-                                                <a href="#" className="profile-link">
-                                                    {PendingFriendRequests.name}
-                                                </a>
-                                                
-                                                <button className="btn btn-primary pull-right" onClick={() => rejectFriendRequest(user.uid)}>Reject</button>
-                                                <button className="btn btn-primary pull-right" onClick={() => acceptFriendRequest(user.uid)}>Accept</button>
-                                            </h5>
-                                        </div>
+                    <h2>Pending Friend Requests</h2>
+                    <ul>
+                        {pendingFriendRequests.map((PendingFriendRequests) => (
+                            <div key={PendingFriendRequests.uid} className="nearby-user">
+                                <div className="row">
+                                    <div className="col-md-2 col-sm-2">
+                                        <img src={PendingFriendRequests.image} alt="friend" className="profile-photo-lg" />
+                                    </div>
+                                    <div className="col-md-7 col-sm-7">
+                                        <h5>
+                                            <a href="#" className="profile-link">
+                                                {PendingFriendRequests.name}
+                                            </a>                                   
+                                            <button className="btn btn-primary pull-right" onClick={() => rejectFriendRequest(user.uid)}>Reject</button>
+                                            <button className="btn btn-primary pull-right" onClick={() => acceptFriendRequest(user.uid)}>Accept</button>
+                                        </h5>
                                     </div>
                                 </div>
-                            ))}
-                        </ul>
-                    </div>
-                ) : (
-                    <p>No Requests!.</p>
-                )}
+                            </div>
+                        ))}
+                    </ul>
                 </div>
-            </div>
+            ) : (
+                <p>No Requests!.</p>
+            )}
             </div>
         </div>
+        </div>
     </div>
-    );
+</div>
+);
 };
 
 export default AcceptRejectFriendRequest;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,157 +1,164 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { UserAuth } from '../context/AuthContext';
 import { auth, db, onAuthStateChanged } from '../firebase';
-import { collection, query, where, getDocs, updateDoc } from "firebase/firestore";
+import { collection, query, where, getDoc, doc, updateDoc } from "firebase/firestore";
 
 const Settings = () => {
-    
     const { user } = UserAuth();
     const collectionUserRef = collection(db, "users");
     const [isButtonClicked, setIsButtonClicked] = useState(false);
-    
     const [profileData, setProfileData] = useState({
-        username: '',
-        email: '',
-        dateOfBirth: '',
-        password: '',
-        address: '',
-        country: '',
-        region: '',
+      username: '',
+      email: '',
+      dateOfBirth: '',
+      password: '',
+      address: '',
+      country: '',
+      region: '',
     });
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged( auth, user => {
-          if (user) {
-            loadProfile(user.uid);
-          }
-        });
-    
-        return () => unsubscribe();
-      }, []);
-    
-      const loadProfile = async (uid) => {
-        try {
-          const q = query(collectionUserRef, where('uid', '==', uid));
-          const querySnapshot = await getDocs(q);
-    
-          if (!querySnapshot.empty) {
-            const userData = querySnapshot.docs[0].data();
-            setProfileData(userData);
-            console.log('Loaded profile data:', userData);
-          } else {
-            console.log('No matching documents for the user');
-          }
-        } catch (error) {
-          console.error('Error loading profile data:', error);
-        }
-      };
   
-      const handleSaveProfile = async () => {
-        try {
-            const q = query(collectionUserRef, where("uid", "==", user.uid));
-            const docs = await getDocs(q);
-
-            if (docs.docs.length !== 0) {
-                const userDocRef = docs.docs[0].ref;
-
-                // Extract fields that are not empty from profileData
-                const updatedProfileData = Object.fromEntries(
-                    Object.entries(profileData)
-                );
-
-                await updateDoc(userDocRef, updatedProfileData);
-                console.log('Profile data updated successfully');
-
-                // Set the button clicked state to true for 1 second
-                setIsButtonClicked(true);
-                setTimeout(() => {
-                    setIsButtonClicked(false);
-                }, 1000);
-            } else {
-                console.log("User not found!");
-            }
-        } catch (error) {
-            console.error('Error updating profile data:', error);
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          loadProfile(user.uid);
         }
+      });
+  
+      return () => unsubscribe();
+    }, []);
+  
+    const loadProfile = async (uid) => {
+      try {
+        const docRef = doc(collectionUserRef, uid);
+        const docSnapshot = await getDoc(docRef);
+  
+        if (docSnapshot.exists()) {
+          const userData = docSnapshot.data();
+          setProfileData(userData);
+          console.log('Loaded profile data:', userData);
+        } else {
+          console.log('No matching documents for the user');
+        }
+      } catch (error) {
+        console.error('Error loading profile data:', error);
+      }
+    };
+  
+    const handleSaveProfile = async () => {
+      try {
+        if (!user) {
+          console.log('User not authenticated');
+          return;
+        }
+  
+        const docRef = doc(collectionUserRef, user.uid);
+        const docSnapshot = await getDoc(docRef);
+  
+        if (docSnapshot.exists()) {
+          const userDocRef = docSnapshot.ref;
+  
+          // Extract fields that are not empty from profileData
+          const updatedProfileData = Object.fromEntries(
+            Object.entries(profileData).filter(([_, value]) => value !== '')
+          );
+  
+          if (Object.keys(updatedProfileData).length > 0) {
+            await updateDoc(userDocRef, updatedProfileData);
+            console.log('Profile data updated successfully');
+  
+            // Set the button clicked state to true for 1 second
+            setIsButtonClicked(true);
+            setTimeout(() => {
+              setIsButtonClicked(false);
+            }, 1000);
+          } else {
+            console.log('No changes to update');
+          }
+        } else {
+          console.log('User not found!');
+        }
+      } catch (error) {
+        console.error('Error updating profile data:', error);
+      }
     };
   
     const handleInputChange = (e) => {
-            const { name, value } = e.target;
-            setProfileData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
+      const { name, value } = e.target;
+      setProfileData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
     };
-
-return (
-    <div className="container d-flex justify-content-center align-items-center">
-        <div className="card">
-            <div className="upper">
-        </div>
-
-        <div className="user text-center">
-            <div className="profile">
-            <img src={user?.photoURL} className="rounded-circle" width="80" alt="Profile" />
+  
+    return (
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="card">
+                <div className="upper">
             </div>
-            <p>{user?.displayName}</p>
-        </div>
-        
-        <div className="p-3 py-5">
-            <div className="d-flex justify-content-between align-items-center mb-3">
-            <h4 className="text-right">Profile Settings</h4>
+    
+            <div className="user text-center">
+                <div className="profile">
+                <img src={user?.photoURL} className="rounded-circle" width="80" alt="Profile" />
+                </div>
+                <p>{user?.displayName}</p>
             </div>
-        
-            <div className="row mt-2">
-            <div className="col-md-6">
-                <label className="labels">Username</label>
-                <input type="text" className="form-control" placeholder={""} name="username" value={profileData?.username} onChange={handleInputChange} />
-            </div> 
-            </div>
-            <div className="row mt-2">
-            <div className="col-md-6">
-                <label className="labels">Email</label>
-                <input required type="text" className="form-control" placeholder={""} name="email" value={profileData?.email} onChange={handleInputChange} />
-            </div>
-            <div className="col-md-6">
-                <label className="labels">Date of Birth</label>
-                <input type="date" className="form-control" placeholder={""} name="dateOfBirth" value={profileData?.dateOfBirth} onChange={handleInputChange} />
-            </div>
-            </div>
-            <div className="row mt-2">
-            <div className="col-md-6">
-                <label className="labels">Password</label>
-                <input type="password" className="form-control" placeholder={""} name="password" value={profileData?.password} onChange={handleInputChange} />
-            </div>
-            <div className="col-md-6">
-                <label className="labels">Repeat Password</label>
-                <input type="password" className="form-control" placeholder={""} name="password" value={""} onChange={handleInputChange} />
-            </div>
-            </div>
-            <div className="row mt-3">
-            <div className="col-md-12">
-                <label className="labels">Address</label>
-                <input type="text" className="form-control" placeholder={""} name="address" value={profileData?.address} onChange={handleInputChange} />
-            </div>
-            <div className="col-md-6">
-                <label className="labels">Country</label>
-                <input type="text" className="form-control" placeholder={""} name="country" value={profileData?.country} onChange={handleInputChange} />
-            </div>
-            <div className="col-md-6">
-                <label className="labels">State/Region</label>
-                <input type="text" className="form-control" placeholder={""} name="region" value={profileData?.region} onChange={handleInputChange} />
-            </div>
-            </div>
-            <div className="mt-5 text-center">
-            <button
-                className={`btn ${isButtonClicked ? 'btn-success' : 'btn-primary'} profile-button`}
-                type="button"
-                onClick={handleSaveProfile}>
-                {isButtonClicked ? 'Profile Saved' : 'Save Profile'}
-            </button>
+            
+            <div className="p-3 py-5">
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                <h4 className="text-right">Profile Settings</h4>
+                </div>
+            
+                <div className="row mt-2">
+                <div className="col-md-6">
+                    <label className="labels">Username</label>
+                    <input type="text" className="form-control" placeholder={""} name="username" value={profileData?.username} onChange={handleInputChange} />
+                </div> 
+                </div>
+                <div className="row mt-2">
+                <div className="col-md-6">
+                    <label className="labels">Email</label>
+                    <input required type="text" className="form-control" placeholder={""} name="email" value={profileData?.email} onChange={handleInputChange} />
+                </div>
+                <div className="col-md-6">
+                    <label className="labels">Date of Birth</label>
+                    <input type="date" className="form-control" placeholder={""} name="dateOfBirth" value={profileData?.dateOfBirth} onChange={handleInputChange} />
+                </div>
+                </div>
+                <div className="row mt-2">
+                <div className="col-md-6">
+                    <label className="labels">Password</label>
+                    <input type="password" className="form-control" placeholder={""} name="password" value={profileData?.password} onChange={handleInputChange} />
+                </div>
+                <div className="col-md-6">
+                    <label className="labels">Repeat Password</label>
+                    <input type="password" className="form-control" placeholder={""} name="password" value={""} onChange={handleInputChange} />
+                </div>
+                </div>
+                <div className="row mt-3">
+                <div className="col-md-12">
+                    <label className="labels">Address</label>
+                    <input type="text" className="form-control" placeholder={""} name="address" value={profileData?.address} onChange={handleInputChange} />
+                </div>
+                <div className="col-md-6">
+                    <label className="labels">Country</label>
+                    <input type="text" className="form-control" placeholder={""} name="country" value={profileData?.country} onChange={handleInputChange} />
+                </div>
+                <div className="col-md-6">
+                    <label className="labels">State/Region</label>
+                    <input type="text" className="form-control" placeholder={""} name="region" value={profileData?.region} onChange={handleInputChange} />
+                </div>
+                </div>
+                <div className="mt-5 text-center">
+                <button
+                    className={`btn ${isButtonClicked ? 'btn-success' : 'btn-primary'} profile-button`}
+                    type="button"
+                    onClick={handleSaveProfile}>
+                    {isButtonClicked ? 'Profile Saved' : 'Save Profile'}
+                </button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 )};
-
+  
 export default Settings;
