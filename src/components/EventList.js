bd96bb18c797cb28232cd27cd7b7a4fca1fbf69e
Ranye Drebes <drebes@simplethings.de>
2024-03-08 14:41:53 +0100

    Api added to Eventlist

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,48 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/eventlist.css';
+import '../styles/eventlist.css'; // Assuming you have styles for this
 
 const EventList = () => {
-  const [events, setEvents] = useState([]);
-  useEffect(() => {
-    // Fetch data from the API endpoint
-    fetch('http://localhost:3001/api/events')
-      .then(response => response.json())
-      .then(data => setEvents(data))
-      .catch(error => console.error('Error fetching Events:', error));
-  }, []);
+    const [events, setEvents] = useState([]);
 
-  return (
-    <div>
-    <h1>Upcoming Events</h1>
-    <ul>
-      {events.map(event => (
-        <li key={event.id}>
-          <div class="container">
-            <div class="row">
-                <div class="col-md-8 offset-md-2">
-                    <div class="event">
-                        <div class="img">
-                            <img alt="Event Image"src="https://www.prepbootstrap.com/Content/images/template/design/design1.jpg" />
+    useEffect(() => {
+        fetch('http://localhost:3001/api/events')
+            .then(response => response.json())
+            .then(data => setEvents(data._embedded.events)) // Extract the events array
+            .catch(error => console.error('Error fetching Events:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Upcoming Events</h1>
+            <ul>
+                {events.map(event => ( 
+                    <li key={event.id}>
+                      <article class="eventlist-event">                 
+                        <div class="eventlist-details">
+                          <h3>{event.name}</h3>
+                          <div class="eventlist-date float-right">
+                  
+                          </div>                 
                         </div>
-                        <div class="desc">
-                            <h3>{event.eventname}</h3>
-                            <div class="social ">
-                            </div>
-                            <a href="#website"><span class="fa fa-globe"></span>  Website</a> 
-                        </div>
-                        <div class="date float-right">
-                            <div class="day">15</div>
-                            <div class="month">Jul</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-  );
+                      </article> 
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default EventList;
