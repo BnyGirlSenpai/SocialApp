81b2ca7f81b9e85bf69d146e1d636cbee6603b05
Ranye Drebes <drebes@simplethings.de>
2024-03-22 10:42:13 +0100

    added unfriend function, added profilpage fields, changed settingspage, and profileSettingspage

---
+++ src/Pages/ProfileSettingsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ProfileSettings from '../components/ProfileSettings'
+
+const ProfileSettingspage = () => {
+  return (
+    <div className='Main'>
+      <Navbar />
+      <ProfileSettings />
+    </div>
+  )
+}
+
+export default ProfileSettingspage
