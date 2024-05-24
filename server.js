de5c7afb70af70f30dc8e68b753df44bd0d7567b
Ranye Drebes <drebes@simplethings.de>
2024-05-24 12:14:22 +0200

    added endpoints for edeting events, updated counters for events, update removing uids from db

--- server.js
+++ server.js
@@ -282,6 +282,62 @@ app.get('/api/events/:uid', async (req, res) => {
     }
 });
 
+// API endpoint to edit event 
+app.get('/api/events/edit/:eid', async (req, res) => {
+    try {
+        let eid = req.params.eid;
+        let [rows] = await connection.query('SELECT event_id, event_name, event_date, event_time, location, description, max_guests_count FROM events WHERE event_id = ?', [eid]);
+        res.status(200).json(rows); 
+        console.log(rows);
+    } catch (error) {
+        console.error('Error retrieving event data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// API endpoint to update Event  data
+app.post('/api/events/edit/update', async (req, res) => {
+    console.log(req.body);
+    let eventData = req.body;
+    let eid = eventData[6]; 
+
+    const selectQuery = 'SELECT COUNT(*) AS count FROM events WHERE event_id = ?';
+    try {
+        let connection = await pool.getConnection();
+        let [results] = await connection.query(selectQuery, [eid]);
+        let eventCount = results[0].count;
+
+        if (eventCount === 1) {
+            let updateFields = [];
+            let updateValues = [];
+            updateFields.push('event_name = ?, location = ?, event_date = ?, event_time = ?, description = ?, max_guests_count = ?'); 
+            
+            if (eventData.length === 7) { 
+                updateValues = eventData.slice(0, 6);
+                updateValues.push(eventData[6]); 
+            } else {
+                console.log('Invalid data format');
+                return res.status(400).json({ error: 'Invalid data format' });
+            }
+            updateValues.push(eid); 
+            let updateQuery = `UPDATE events SET ${updateFields} WHERE event_id = ?`;
+
+            await connection.query(updateQuery, updateValues);
+            console.log('Event data updated');
+            res.status(200).json({ success: true, message: 'Event data updated' });
+        } else {
+            console.log('Event not found');
+            res.status(404).json({ error: 'Event not found' });
+        }
+    } catch (error) {
+        console.error('Error processing data:', error);
+        res.status(500).json({ error: 'Failed to process data' });
+    }
+    finally {
+        connection.release(); // Ensure connection is released in case of errors
+    }
+})
+
 // API to update event database after invited_guest interaction
 app.post('/api/events/update/', async (req, res) => {
     try {
@@ -334,19 +390,23 @@ app.post('/api/events/update/', async (req, res) => {
                                             '$',
                                             JSON_OBJECT('uid', ?)
                                         ),
-                    invited_guests = JSON_REMOVE(
-                                        IFNULL(invited_guests, '[]'),
-                                        JSON_UNQUOTE(
-                                            JSON_SEARCH(
-                                                IFNULL(invited_guests, '[]'),
-                                                'one',
-                                                ?,
-                                                NULL,
-                                                '$[*].uid'
+                    invited_guests = IF(
+                                        JSON_LENGTH(IFNULL(invited_guests, '[]')) > 1,
+                                        JSON_REMOVE(
+                                            IFNULL(invited_guests, '[]'),
+                                            JSON_UNQUOTE(
+                                                JSON_SEARCH(
+                                                    IFNULL(invited_guests, '[]'),
+                                                    'one',
+                                                    ?,
+                                                    NULL,
+                                                    '$[*].uid'
+                                                )
                                             )
-                                        )
+                                        ),
+                                        '[]'
                                     )
-                WHERE event_id = ?
+                WHERE event_id = ?;           
             `, [uid_guest, uid_guest, event_id]);
 
             // Update counts
@@ -364,20 +424,25 @@ app.post('/api/events/update/', async (req, res) => {
                                         '$',
                                         JSON_OBJECT('uid', ?)
                                     ),
-                joined_guests = JSON_REMOVE(
-                                    IFNULL(joined_guests, '[]'),
-                                    JSON_UNQUOTE(
-                                        JSON_SEARCH(
-                                            IFNULL(joined_guests, '[]'),
-                                            'one',
-                                            ?,
-                                            NULL,
-                                            '$[*].uid'
+                joined_guests =  IF(
+                                    JSON_LENGTH(IFNULL(invited_guests, '[]')) > 1,
+                                    JSON_REMOVE(
+                                        IFNULL(invited_guests, '[]'),
+                                        JSON_UNQUOTE(
+                                            JSON_SEARCH(
+                                                IFNULL(invited_guests, '[]'),
+                                                'one',
+                                                ?,
+                                                NULL,
+                                                '$[*].uid'
+                                            )
                                         )
-                                    )
+                                    ),
+                                    '[]'
                                 )
-            WHERE event_id = ?
+                WHERE event_id = ?;           
             `, [uid_guest, uid_guest, event_id]);
+            await updateCounts(event_id, connection);
 
             await connection.commit();
         }
@@ -392,6 +457,7 @@ app.post('/api/events/update/', async (req, res) => {
     }
 });
 
+// Update User Counters
 async function updateCounts(event_id, connection) {
     const [guestCounts] = await connection.query(`
         SELECT 
